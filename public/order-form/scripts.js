ef870ca8fbaf341d5badbafb85abcf03c71abd69
Marie Foxall <marie@wastedeffort.ca>
2020-07-07 16:04:10 -0400

    Node Workshop 4

--- public/order-form/scripts.js
+++ public/order-form/scripts.js
@@ -1,38 +1,38 @@
 const orderItems = {
-  undefined: { label: 'Pick an item', imgUrl: './assets/question.jpg' },
-  bottle: { label: 'Bottle', imgUrl: './assets/bottle.png' },
-  shirt: { label: 'T-shirt', imgUrl: './assets/tshirt.png' },
-  socks: { label: 'Socks', imgUrl: './assets/socks.jpg' },
+  undefined: { label: "Pick an item", imgUrl: "./assets/question.jpg" },
+  bottle: { label: "Bottle", imgUrl: "./assets/bottle.png" },
+  shirt: { label: "T-shirt", imgUrl: "./assets/tshirt.png" },
+  socks: { label: "Socks", imgUrl: "./assets/socks.jpg" },
 };
 const errorMessages = {
-  unavailable: 'Item out of stock. :(',
-  'repeat-customer':
-    'Exiting user. Not allowed to place another order at the moment.',
-  undeliverable: 'Outside of delivery zone. :(',
-  'missing-data': "Oops! Looks like we're missing some information.",
+  unavailable: "Item out of stock. :(",
+  "repeat-customer":
+    "Existing user. Not allowed to place another order at the moment.",
+  undeliverable: "Outside of delivery zone. :(",
+  "missing-data": "Oops! Looks like we're missing some information.",
 };
-const submitButton = document.getElementById('confirm-button');
-const order = document.getElementById('order');
-const errorMsg = document.getElementById('error');
-const size = document.getElementById('size');
-const givenName = document.getElementById('givenName');
-const surname = document.getElementById('surname');
-const email = document.getElementById('email');
-const address = document.getElementById('address');
-const city = document.getElementById('city');
-const province = document.getElementById('province');
-const postcode = document.getElementById('postcode');
-const country = document.getElementById('country');
+const submitButton = document.getElementById("confirm-button");
+const order = document.getElementById("order");
+const errorMsg = document.getElementById("error");
+const size = document.getElementById("size");
+const givenName = document.getElementById("givenName");
+const surname = document.getElementById("surname");
+const email = document.getElementById("email");
+const address = document.getElementById("address");
+const city = document.getElementById("city");
+const province = document.getElementById("province");
+const postcode = document.getElementById("postcode");
+const country = document.getElementById("country");
 
 const updateForm = () => {
-  const sel = document.getElementById('order').value;
-  if (sel === 'shirt') {
-    document.getElementById('sizing').style.display = 'flex';
+  const sel = document.getElementById("order").value;
+  if (sel === "shirt") {
+    document.getElementById("sizing").style.display = "flex";
   } else {
-    document.getElementById('sizing').style.display = 'none';
+    document.getElementById("sizing").style.display = "none";
   }
   document.getElementById(
-    'order-image'
+    "order-image"
   ).style.backgroundImage = `url(${orderItems[sel].imgUrl}`;
 };
 
@@ -48,7 +48,7 @@ const handleSubmit = (event) => {
     size: size.value,
     givenName: givenName.value,
     surname: surname.value,
-    email: email,
+    email: email.value,
     address: address.value,
     city: city.value,
     province: province.value,
@@ -56,22 +56,24 @@ const handleSubmit = (event) => {
     country: country.value,
   };
 
-  fetch('/order', {
-    method: 'POST',
+  fetch("/order", {
+    method: "POST",
     body: JSON.stringify(data),
     headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
+      Accept: "application/json",
+      "Content-Type": "application/json",
     },
   })
-    .then((res) => res.json())
+    .then((response) => {
+      return response.json();
+    })
     .then((data) => {
       const { status, error } = data;
-      if (status === 'success') {
-        window.location.href = '/order-confirmed';
+      if (status === "success") {
+        window.location.href = `/order-confirmed/?customerName=${givenName.value}&customerOrder=${order.value}&customerProvince=${province.value}&customerSize=${size.value}`;
       } else if (error) {
         submitButton.disabled = false;
-        errorMsg.style.display = 'flex';
+        errorMsg.style.display = "flex";
         errorMsg.innerText = errorMessages[error];
       }
     });
--- server.js
+++ server.js
@@ -1,25 +1,77 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const { stock, customers } = require("./data/promo");
 
 express()
   .use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Origin", "*");
     res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
     );
     next();
   })
-  .use(morgan('tiny'))
-  .use(express.static('public'))
+  .use(morgan("tiny"))
+  .use(express.static("public"))
   .use(bodyParser.json())
   .use(express.urlencoded({ extended: false }))
-  .set('view engine', 'ejs')
+  .set("view engine", "ejs")
 
   // endpoints
 
-  .get('*', (req, res) => res.send('Dang. 404.'))
+  .post("/order", (req, res) => {
+    console.log(req.body);
+    if (req.body.order === "bottle" || req.body.order === "socks") {
+      req.body.size = "no size";
+    }
+    console.log(req.body);
+    customers.forEach((customer) => {
+      if (
+        (req.body.givenName === customer.givenName &&
+          req.body.surname === customer.surname) ||
+        req.body.email === customer.email ||
+        req.body.address === customer.address
+      ) {
+        res.status(403).json({ status: "error", error: "repeat-customer" });
+      } else if (req.body.country !== "Canada") {
+        res.status(403).json({ status: "error", error: "undeliverable" });
+      } else if (Object.values(req.body).includes("undefined")) {
+        res.status(403).json({ status: "error", error: "missing-data" });
+      } else if (
+        Number(stock[`${req.body.order}`]) === 0 ||
+        Number(stock[`${req.body.order}`][`${req.body.size}`]) === 0
+      ) {
+        res.status(403).json({ status: "error", error: "unavailable" });
+      } else {
+        res.status(200).json({ status: "success" });
+      }
+    });
+  })
+
+  .get("/order-confirmed", (req, res) => {
+    console.log(req.query);
+    // console.log(queryString);
+    console.log("hello");
+    res.status(200);
+    // res.send("everything is cool, great order buddy!");
+    let customerOrder = "";
+    if (req.query.customerOrder === "bottle") {
+      customerOrder = "a bottle";
+    } else if (req.query.customerOrder === "socks") {
+      customerOrder = "a pair of socks";
+    } else if (req.query.customerOrder === "shirt") {
+      customerOrder = `a shirt in size ${req.query.customerSize}`;
+    }
+
+    res.render("order-confirmed", {
+      customerName: req.query.customerName,
+      customerOrder: customerOrder,
+      customerProvince: req.query.customerProvince,
+    });
+  })
+
+  .get("*", (req, res) => res.send("Dang. 404."))
   .listen(8000, () => console.log(`Listening on port 8000`));
